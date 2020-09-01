932a37076794a59170f7f014fc246d1a224ce8a4
lmachens <leon.machens@googlemail.com>
2020-09-01 21:48:57 +0200

    Fix tests

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import Page from "./pages/Page";
 function App() {
   return (
     <div className="App">
-      <Page />
+      <Page
+        onLogin={() => console.log("login")}
+        onLogout={() => console.log("logout")}
+        onCreateAccount={() => console.log("create account")}
+      />
       <a href="/storybook">To Storybook</a>
     </div>
   );
