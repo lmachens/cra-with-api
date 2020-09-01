f2aad755d0cb44aa0de85cec76c8790272247ad5
lmachens <leon.machens@googlemail.com>
2020-09-01 20:43:45 +0200

    Serve client and storybook

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(port, () => {
