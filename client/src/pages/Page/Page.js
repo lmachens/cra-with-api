0089f5c9e0869f19f14757b657562e9890b3e78d
lmachens <leon.machens@googlemail.com>
2020-09-01 21:36:07 +0200

    Move storybook samples

--- client/src/pages/Page/Page.js
+++ client/src/pages/Page/Page.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Header } from "./Header";
+import Header from "../../components/Header";
 import "./page.css";
 
-export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
+const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <article>
     <Header
       user={user}
@@ -91,3 +91,5 @@ Page.propTypes = {
 Page.defaultProps = {
   user: null,
 };
+
+export default Page;
