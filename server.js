88e873d94767e9b6fadc8986ba2d7e1daf898c9c
lmachens <leon.machens@googlemail.com>
2020-09-01 20:19:56 +0200

    Read port from env https://devcenter.heroku.com/articles/runtime-principles#web-servers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
