b359c2bb07a412cc8a3a795c6a218203556b70e5
lmachens <leon.machens@googlemail.com>
2020-09-01 21:48:57 +0200

    Fix tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders storybook react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/To Storybook/i);
   expect(linkElement).toBeInTheDocument();
 });
